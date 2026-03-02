491129b35d252cf919faa2b197a4b85597fffed5
phongtranWW <tranphong.programmer@gmail.com>
2026-03-03 06:43:11 +0700

    feat: setup postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import postgresConfig from './config/postgres.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [postgresConfig],
     }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        const config = configService.get<DataSourceOptions>('postgres');
+        if (!config) throw new Error('Postgres config not found');
+        return config;
+      },
+      inject: [ConfigService],
+    }),
+    UserModule,
   ],
 })
 export class AppModule {}
