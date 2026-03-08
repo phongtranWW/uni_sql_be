74a1b7a2dfff2c5174201937db95fd1989462dfb
phongtranWW <tranphong.programmer@gmail.com>
2026-03-08 13:23:48 +0700

    feat: setup cá»ors

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const configService = app.get(ConfigService);
+  const corsOptions = configService.getOrThrow<CorsOptions>('cors');
+  app.enableCors(corsOptions);
 
   const config = new DocumentBuilder()
     .setTitle('Uni SQL API')
